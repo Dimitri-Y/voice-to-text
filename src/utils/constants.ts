91f4d59d97bea43eab5c8391fcd09a5c7b01a088
Dimitri-Y <dimoon.bond@gmail.com>
2024-12-20 01:28:55 +0200

    upprove file size 25MB to 5MB, refactor constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const MAX_FILE_SIZE_TEXT = 5;
+export const MAX_FILE_SIZE = MAX_FILE_SIZE_TEXT * 1024 * 1024;
+export const ALLOWED_AUDIO_TYPES = ["audio/mp3", "audio/mpeg", "audio/mp4", "audio/wav", "audio/x-m4a", "audio/m4a"]
